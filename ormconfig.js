4c66a0b2420ae81db6a450f16ccbe069352b61bf
Rafael Lemos <rafalemos3232@gmail.com>
2020-04-11 01:35:18 -0300

    add: pg typeorm connection with env setup and create ormconfig

---
+++ ormconfig.js
@@ -0,0 +1,23 @@
+const { DATABASE_HOST, DATABASE_PORT, DATABASE_USER, DATABASE_PASSWORD, DATABASE } = process.env;
+
+module.exports = {
+    type: "postgres",
+
+    database: DATABASE,
+    host: DATABASE_HOST,
+    password: DATABASE_PASSWORD,
+    port: DATABASE_PORT,
+    username: DATABASE_USER,
+
+    cli: {
+      migrationsDir: "src/migrations",
+      entitiesDir: "src/entity",
+      subscribersDir: "src/subscriber"
+    },
+    entities: [
+        "src/entity/**/*.ts"
+    ],
+    migrations: [
+        "src/migration/**/*.ts"
+    ],
+};
---
+++ src/migrations/1586579617734-create-user.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createUser1586579617734 implements MigrationInterface {
+    name = 'createUser1586579617734'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`, undefined);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "user"`, undefined);
+    }
+
+}
